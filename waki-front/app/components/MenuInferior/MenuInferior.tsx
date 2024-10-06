97135fafda499679f5f09c7211175e377ffb4545
FedericoMontero <fede17.m@gmail.com>
2024-10-06 00:18:00 -0300

    componente menu 2/3

--- waki-front/app/components/MenuInferior/MenuInferior.tsx
+++ waki-front/app/components/MenuInferior/MenuInferior.tsx
@@ -3,26 +3,29 @@ import React from 'react'
 import ScoutPlayers from '../../assets/Vector-2.png'
 import Partidos from '../../assets/Vector-1.png'
 import Divisiones from '../../assets/Vector.png'
+import Image from 'next/image'
+
 
 const MenuInferior = () => {
   
   return (
-    <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
-    <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
-        <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-            <img src='/assets/Vector.png' alt="ScoutPlayers" className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" />
-            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Home</span>
-        </button>
-        <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-        <img src={Partidos} alt="ScoutPlayers" className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" />
-            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Wallet</span>
-        </button>
-        <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-        <img src={Divisiones} alt="ScoutPlayers" className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" />
-            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Settings</span>
-        </button>
-    </div>
+<div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-custom-blue border-t border-white-200 dark:bg-custom-blue dark:border-white-600">
+  <div className="grid h-full max-w-lg grid-cols-3 mx-auto font-medium">
+    <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-custom-blue dark:hover:bg-white-800 group">
+      <Image src={ScoutPlayers} width={20} height={20} alt="ScoutPlayers" className="w-5 h-5 mb-2 text-white-500 dark:text-white-400 group-hover:text-white-600 dark:group-hover:text-white-500" />
+      <span className="text-sm text-white-500 dark:text-white-400 group-hover:text-white-600 dark:group-hover:text-white-500">Scout Players</span>
+    </button>
+    <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-custom-blue dark:hover:bg-white-800 group">
+      <Image src={Partidos} width={20} height={20} alt="ScoutPlayers" className="w-5 h-5 mb-2 text-white-500 dark:text-white-400 group-hover:text-white-600 dark:group-hover:text-white-500" />
+      <span className="text-sm text-white-500 dark:text-white-400 group-hover:text-white-600 dark:group-hover:text-white-500">Partidos</span>
+    </button>
+    <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-custom-blue dark:hover:bg-white-800 group">
+      <Image src={Divisiones} width={20} height={20} alt="ScoutPlayers" className="w-5 h-5 mb-2 text-white-500 dark:text-white-400 group-hover:text-white-600 dark:group-hover:text-white-500" />
+      <span className="text-sm text-white-500 dark:text-white-400 group-hover:text-white-600 dark:group-hover:text-white-500">Divisiones</span>
+    </button>
+  </div>
 </div>
+
   )
 }
 
