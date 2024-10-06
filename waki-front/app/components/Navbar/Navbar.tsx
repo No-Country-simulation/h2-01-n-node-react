6109836c69415741e357019d9a4112f3bce344b0
Alan Rios <riosalan264@gmail.com>
2024-10-05 23:49:05 -0300

    fix: refactor navbar in general component pages

--- waki-front/app/components/Navbar/Navbar.tsx
+++ waki-front/app/components/Navbar/Navbar.tsx
@@ -2,44 +2,43 @@
 
 import React, { useState } from "react";
 
-export default function Header() {
-  const [activeTab, setActiveTab] = useState("InicioSesion");
+type Tab = {
+  id: string;
+  label: string;
+};
 
-  const handleTabClick = (tab: React.SetStateAction<string>) => {
-    setActiveTab(tab);
+type HeaderProps = {
+  tabs: Tab[];
+  onTabChange: (tabId: string) => void;
+};
+
+export default function Header({ tabs, onTabChange }: HeaderProps) {
+  const [activeTab, setActiveTab] = useState(tabs[0]?.id);
+
+  const handleTabClick = (tabId: string) => {
+    setActiveTab(tabId);
+    onTabChange(tabId);
   };
 
   return (
     <div className="text-sm font-medium text-center text-gray-500 border-b-4 border-gray-200 dark:text-gray-400 dark:border-gray-700 mb-2">
       <ul className="flex flex-wrap justify-center md:justify-start -mb-px">
-        <li className="flex-grow">
-          <a
-            href="#"
-            onClick={() => handleTabClick("InicioSesion")}
-            style={{ position: "relative", top: 3 }}
-            className={`inline-block p-4 border-b-4 ${
-              activeTab === "InicioSesion"
-                ? "text-[#317EF4] border-[#317EF4]"
-                : "border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-            } rounded-t-lg w-full text-center`}
-          >
-            Iniciar Sesion
-          </a>
-        </li>
-        <li className="flex-grow">
-          <a
-            href="#"
-            onClick={() => handleTabClick("Registrate")}
-            style={{ position: "relative", top: 3 }}
-            className={`inline-block p-4 border-b-4 ${
-              activeTab === "Registrate"
-                ? "text-[#317EF4] border-[#317EF4]"
-                : "border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-            } rounded-t-lg w-full text-center`}
-          >
-            Registrate
-          </a>
-        </li>
+        {tabs.map((tab) => (
+          <li key={tab.id} className="flex-grow">
+            <a
+              href="#"
+              onClick={() => handleTabClick(tab.id)}
+              style={{ position: "relative", top: 3 }}
+              className={`inline-block p-4 border-b-4 ${
+                activeTab === tab.id
+                  ? "text-[#317EF4] border-[#317EF4]"
+                  : "border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+              } rounded-t-lg w-full text-center`}
+            >
+              {tab.label}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
