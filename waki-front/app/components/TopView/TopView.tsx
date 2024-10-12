afac2b6062a94ca2117009d0227b21c74ec08cf2
FedericoMontero <fede17.m@gmail.com>
2024-10-11 23:15:54 -0300

    111024

--- waki-front/app/components/TopView/TopView.tsx
+++ waki-front/app/components/TopView/TopView.tsx
@@ -1,11 +1,41 @@
 "use client"
 
 import React from 'react'
+import Avatar from '@/app/assets/avatar/people1.jpeg'
+import IconBall from '@/app/assets/icon-ball.png'
+import Image from "next/image";
+import './topview.css';
+
+
+
 
 export default function TopView() {
   return (
     <div>
-        <h1>Componente1</h1>
+        <div className="component-imagen">
+        <Image
+        priority={true}
+        alt="Imagen de Perfil"
+        className="avatarImg"
+        src={Avatar}
+        width={50}
+        height={50}
+        />
+        <div className='welcome-user'>¡Hola, Diego! </div>
+        <div className='icon-ball-container'>
+        <Image
+        priority={true}
+        alt="Imagen de Perfil"
+        className="icon-ball"
+        src={IconBall}
+        width={35}
+        height={35}
+
+        />
+          <div className='counter-life'>5</div>
+          <button className='buy-button'>+</button>
+        </div>
+      </div>
     </div>
   )
 }
