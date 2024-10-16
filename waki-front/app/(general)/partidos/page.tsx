b88a7ada12938dbf14e6c176b413ec4060ed33a1
Alan Rios <riosalan264@gmail.com>
2024-10-15 22:32:41 -0300

    fix: refactor styles in carrousel partidos

--- waki-front/app/(general)/partidos/page.tsx
+++ waki-front/app/(general)/partidos/page.tsx
@@ -1,15 +1,15 @@
-"use client"
-import MenuInferior from '@/app/components/MenuInferior/MenuInferior'
-import SplashScreen from '@/app/components/SplashScreen/SplashScreen'
-import TopView from '@/app/components/TopView/TopView'
-import Carrusel from '@/app/components/Carrusel/Carrusel'
-import ChipsFilter from '@/app/components/ChipsFilter/ChipsFilter'
+"use client";
+import MenuInferior from "@/app/components/MenuInferior/MenuInferior";
+import SplashScreen from "@/app/components/SplashScreen/SplashScreen";
+import TopView from "@/app/components/TopView/TopView";
+import Carrusel from "@/app/components/Carrusel/Carrusel";
+import ChipsFilter from "@/app/components/ChipsFilter/ChipsFilter";
 import { FiCalendar } from "react-icons/fi";
-import MatchCard from '@/app/components/MatchCard/MatchCard'
-import MatchCardLive from '@/app/components/MatchCardLive/MatchCardLive'
+import MatchCard from "@/app/components/MatchCard/MatchCard";
+import MatchCardLive from "@/app/components/MatchCardLive/MatchCardLive";
 import Collapse from "@/app/components/Collapse/Collapse";
 import { useState } from "react";
-import './partidos.css'
+import "./partidos.css";
 import Header from "@/app/components/Navbar/Navbar";
 import {
   Select,
@@ -20,13 +20,11 @@ import {
 } from "@mui/material";
 import "@/app/(general)/ligas/ligas.css";
 
-import React from 'react'
-
+import React from "react";
+import Filter from "@/app/components/Filter/Filter";
 
 export default function page() {
-
   const [activeTab, setActiveTab] = useState("Hoy");
-  const [filter, setFilter] = useState("tendencias");
 
   const formatDate = (date: Date) => {
     const day = date.getDate();
@@ -55,22 +53,24 @@ export default function page() {
     setFilter(event.target.value as string);
   };
 
-
   return (
-    <div className='container-partido'>
-      {/*<MenuInferior/>*/}
-      {/*<SplashScreen/>*/}
-      <TopView/>
-      <h1 className='title'>¡No te pierdas tus favoritos!</h1>
-      <Carrusel/>
-      <h1 className='partidosTitle'>Partidos</h1>
-      <div ><ChipsFilter/></div>
+    <div className="container-partido">
+      <TopView />
+      <h1 className="title">¡No te pierdas tus favoritos!</h1>
+      <Carrusel />
+      <div className="flex items-center justify-between title-container">
+        <h1 className="partidosTitle">Partidos</h1>
+        <Filter />
+      </div>
+      <div>
+        <ChipsFilter />
+      </div>
       <Header tabs={tabs} onTabChange={handleTabChange} />
-      <h1 className='statePartido'>En vivo    ___________________________</h1>
-      <MatchCardLive/>
-      <h1 className='statePartido'>Por Jugar    _________________________</h1>
-      <MatchCard/>
+      <h1 className="statePartido">En vivo ___________________________</h1>
+      <MatchCardLive />
+      <h1 className="statePartido">Por Jugar _________________________</h1>
+      <MatchCard />
       <MenuInferior />
     </div>
-  )
+  );
 }
