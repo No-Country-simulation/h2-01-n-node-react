0e0274600adfb00c7c6dbda83725f5a15f2e2fb6
Alan Rios <riosalan264@gmail.com>
2024-10-21 19:30:50 -0300

    fix: fix errors impors in production deploy

--- waki-front/app/(general)/testUI/page.tsx
+++ waki-front/app/(general)/testUI/page.tsx
@@ -1,13 +1,9 @@
-import MatchCard from '@/app/components/MatchCard/MatchCard'
 import BotomChat from '@/app/components/BotomChat/BotomChat'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
-      {/*<MenuInferior/>*/}
-      {/*<SplashScreen/>*/}
-      {/*<ChipsFilter/>*/}
       <BotomChat/>
     </div>
   )
--- waki-front/app/components/BotomChat/BotomChat.tsx
+++ waki-front/app/components/BotomChat/BotomChat.tsx
@@ -1,43 +1,33 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import './botomchat.css'
-import Link from 'next/link'
-import { Button } from "@/components/ui/button"
-import { MessageCircleIcon } from 'lucide-react'
-import { usePathname, useRouter } from 'next/navigation'
-import { IoChatbubbleOutline } from "react-icons/io5";
-import { IoHomeOutline } from "react-icons/io5";
+"use client";
+import React, { useState } from "react";
+import "./botomchat.css";
+import { useRouter } from "next/navigation";
 import { IoChatbubblesOutline } from "react-icons/io5";
-
-
-
 
 export default function BotomChat() {
 
-    const pathname = usePathname()
-    const router = useRouter()
-    const [activeIcon, setActiveIcon] = useState<string>("faqbot")
+  const router = useRouter();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [activeIcon, setActiveIcon] = useState<string>("faqbot");
 
   const handleIconClick = (icon: string) => {
     if (icon === "faqbot") {
-      
-      router.push('/faqbot') 
+      router.push("/faqbot");
     }
-    setActiveIcon(icon)
-  }
+    setActiveIcon(icon);
+  };
   return (
     <div>
-    <div className="fixed bottom-6 right-6">
-    <button
-  className="bg-black rounded-full w-16 h-16 shadow-lg hover:shadow-xl transition-shadow duration-300 flex items-center justify-center"
-  onClick={() => handleIconClick("faqbot")} id='btmColor'
->
-  <IoChatbubblesOutline className='w-8 h-8 text-white' />
-  <span className="sr-only">Ir al chat FAQ</span>
-</button>
+      <div className="fixed bottom-6 right-6">
+        <button
+          className="bg-black rounded-full w-16 h-16 shadow-lg hover:shadow-xl transition-shadow duration-300 flex items-center justify-center"
+          onClick={() => handleIconClick("faqbot")}
+          id="btmColor"
+        >
+          <IoChatbubblesOutline className="w-8 h-8 text-white" />
+          <span className="sr-only">Ir al chat FAQ</span>
+        </button>
+      </div>
     </div>
-
-    </div>
-
-  )
+  );
 }
