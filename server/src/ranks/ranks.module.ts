3f1d3c7b6ada72c12c1073e22f7fa8212301012f
jp-quintana <juanquintana1996@gmail.com>
2024-10-28 17:11:51 -0300

    feat: add ranks update

--- server/src/ranks/ranks.module.ts
+++ server/src/ranks/ranks.module.ts
@@ -3,9 +3,10 @@ import { RanksService } from './ranks.service';
 import { RanksController } from './ranks.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Ranks from './ranks.entities';
+import { Users } from 'src/users/users.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ranks])],
+  imports: [TypeOrmModule.forFeature([Ranks, Users])],
   providers: [RanksService],
   controllers: [RanksController],
 })
--- server/src/ranks/ranks.service.ts
+++ server/src/ranks/ranks.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
+import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
 import Ranks from './ranks.entities';
-import { Repository } from 'typeorm';
+import { EntityManager, Repository } from 'typeorm';
+import { Users } from 'src/users/users.entity';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class RanksService {
   constructor(
     @InjectRepository(Ranks)
     private ranksRepository: Repository<Ranks>,
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>,
+    @InjectEntityManager() private readonly entityManager: EntityManager,
   ) {}
 
   async findAll() {
@@ -26,5 +31,63 @@ export class RanksService {
       .getOne();
 
     return rank;
+  }
+
+  @Cron('55 23 * * *', {
+    timeZone: 'America/Argentina/Buenos_Aires',
+  })
+  async updateUserRanks() {
+    const highestScore = await this.usersRepository
+      .createQueryBuilder('user')
+      .select('user.points', 'points')
+      .orderBy('user.points', 'DESC')
+      .take(1)
+      .getRawOne();
+
+    if (!highestScore) throw new Error('Error updating user ranks');
+
+    const silverTopLimit = Math.floor((highestScore.points / 3) * 2);
+    const silverBottomLimit = Math.floor(highestScore.points / 3);
+    const bronzeTopLimit = silverBottomLimit;
+    const goldBottomLimit = silverTopLimit;
+
+    try {
+      await this.entityManager.transaction(
+        async (transactionManager: EntityManager) => {
+          await transactionManager.update(
+            Ranks,
+            { name: 'Bronze' },
+            { topLimit: bronzeTopLimit },
+          );
+          await transactionManager.update(
+            Ranks,
+            { name: 'Silver' },
+            { bottomLimit: silverBottomLimit, topLimit: silverTopLimit },
+          );
+          await transactionManager.update(
+            Ranks,
+            { name: 'Gold' },
+            { bottomLimit: goldBottomLimit },
+          );
+
+          await transactionManager
+            .createQueryBuilder()
+            .update(Users)
+            .set({
+              rank: () => {
+                return `CASE 
+            WHEN points < ${silverBottomLimit} THEN 'Bronze'
+            WHEN points >= ${silverBottomLimit} AND points < ${silverTopLimit} THEN 'Silver'
+            WHEN points >= ${silverTopLimit} THEN 'Gold'
+          END`;
+              },
+            })
+            .execute();
+        },
+      );
+      console.log('User ranks updated successfully');
+    } catch (error: any) {
+      if (!highestScore) throw new Error(`Error updating user ranks: ${error}`);
+    }
   }
 }
