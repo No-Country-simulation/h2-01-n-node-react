f2166b52a16737213bb3271dfa316c579e9ab80a
FedericoMontero <fede17.m@gmail.com>
2024-10-30 21:12:55 -0300

    pantalla notificaciones

---
+++ waki-front/app/(general)/notificaciones/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+import React from 'react'
+import './notificaciones.css'
+import Flecha from '@/app/assets/flecha.png'
+import AjusteIcon from '@/app/assets/ajusteIcon.svg'
+import { useState, useEffect } from "react";
+import Image from "next/image";
+
+export default function page() {
+  return (
+    <div>
+      <div className='topview'>
+      <Image
+              src={Flecha}
+              alt="Flecha hacia atras"
+              className="flechaImg mr-5"
+            />
+        <h1 className='title-topview'>Notificaciones</h1>
+        <Image
+              src={AjusteIcon}
+              alt="Icono de ajuste"
+              className="ajusteIcon"
+            />
+      </div>
+
+      <div className='container-new'>
+        <h1 className='subtitle-container'>Nuevas</h1>
+
+        
+      </div>  
+
+    </div>
+  )
+}
